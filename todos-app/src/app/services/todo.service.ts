684ce3265cfa1b5f922056d283beda5cd8e40482
philables <phillipables1@gmail.com>
2017-12-29 22:20:55 -0500

    hard code todo.service constructor test

--- todos-app/src/app/services/todo.service.ts
+++ todos-app/src/app/services/todo.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Todo } from '../classes/todo'
 
 @Injectable()
 export class TodoService {
 
-  constructor() { }
+  private todos: Todo[];
+  private nextId: number;
+
+  constructor() { 
+    this.todos = [
+      new Todo(0, "Make dinner tonight!"),
+      new Todo(1, "Fold the laundry."),
+      new Todo(2, "Learn to make an Angular app!")
+    ];
+
+    this.nextId = 3;
+  }
 
 }
